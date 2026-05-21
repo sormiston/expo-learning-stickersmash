8afd3b59d6088d2e2ff3bbfcdcf7708639924070
sormiston <sean.david.ormiston@gmail.com>
2026-05-21 19:33:43 +0200

    init + assets + start routing

---
+++ app/about/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+    gap: 30,
+  },
+  text: {
+    color: "#fff",
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about" style={styles.button}>
+        Go to about About screen
+      </Link>
+    </View>
+  );
+}
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+    gap: 30
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
