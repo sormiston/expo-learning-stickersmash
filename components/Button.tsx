c3ad4316db7ac5cb3ae889b78afc250581772c41
sormiston <sean.david.ormiston@gmail.com>
2026-05-27 12:32:17 +0200

    adds button primary theme

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,41 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 type ButtonProps = {
   label: string;
+  theme?: "primary";
 };
 
-export default function Button({ label }: ButtonProps) {
+export default function Button({ label, theme }: ButtonProps) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          {
+            borderWidth: 4,
+            borderColor: "#ffd33d",
+            borderRadius: 18,
+          },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={() => alert("You pressed a button")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
   return (
     <View style={styles.buttonContainer}>
       <Pressable
@@ -37,5 +68,8 @@ const styles = StyleSheet.create({
   buttonLabel: {
     color: "#fff",
     fontSize: 16,
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
 });
